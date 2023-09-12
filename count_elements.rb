dfe3b5c98dc23e89295f2867752a01e43e8eb05a
multikasia <kasia.ekiert@gmail.com>
2023-09-12 14:11:40 +0200

    solutions

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,11 @@
 class CountElements
+  def initialize(elements)
+    @elements = elements
+  end
 
+  def perform
+    @elements.each_with_object(Hash.new(0)) do |element, count_hash|
+      count_hash[element] += 1
+    end
+  end
 end
--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,17 @@
 class ValidatePassword
+  MAX_LENGTH = 24
 
+  def initialize(password)
+    @password = password
+  end
+
+  def perform
+    return false unless @password.match?(/[A-Z]/) 
+    return false unless @password.match?(/\d/) 
+    return false unless @password.match?(/[^A-Za-z0-9]/)
+    return false if @password.match?(/(.)\1\1/)
+    return false if @password.length > MAX_LENGTH
+
+    true
+  end
 end
